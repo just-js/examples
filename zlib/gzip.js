b0019464af7d5f95fb526dab391826013bf3c303
Andrew Johnston <apjohnsto@gmail.com>
2020-09-01 19:22:30 +0100

    update gzip example

--- zlib/gzip.js
+++ zlib/gzip.js
@@ -1,25 +1,26 @@
 const { net, sys } = just
 const zlib = require('./zlib.js')
+const { STDIN_FILENO, STDOUT_FILENO } = sys
 
 const BUFSIZE = 64 * 1024
 
 const buf = new ArrayBuffer(BUFSIZE)
 const deflate = zlib.createDeflate(buf, BUFSIZE, zlib.Z_DEFAULT_COMPRESSION, 31)
-let len = net.read(0, buf)
+let len = net.read(STDIN_FILENO, buf)
 let written = 0
 while (len > 0) {
   written = zlib.writeDeflate(deflate, len, zlib.Z_NO_FLUSH)
   if (written < 0) throw new Error(`writeDeflate: ${written}`)
   if (written > 0) {
-    written = net.write(1, deflate, written)
+    written = net.write(STDOUT_FILENO, deflate, written)
     if (written < 0) throw new Error(`net.write: ${written} (${sys.errno()}) ${sys.strerror(sys.errno())}`)
   }
-  len = net.read(0, buf)
+  len = net.read(STDIN_FILENO, buf)
 }
 written = zlib.writeDeflate(deflate, 0, zlib.Z_FINISH)
 if (written < 0) throw new Error(`writeDeflate: ${written}`)
 if (written > 0) {
-  written = net.write(1, deflate, written)
+  written = net.write(STDOUT_FILENO, deflate, written)
   if (written < 0) throw new Error(`net.write: ${written} (${sys.errno()}) ${sys.strerror(sys.errno())}`)
 }
 zlib.endDeflate(deflate, true)
