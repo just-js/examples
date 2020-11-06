c5c91346ee6aa8139ed4ee193610c9b8ea720f3b
Andrew Johnston <apjohnsto@gmail.com>
2020-11-06 02:41:55 +0000

    fir memory usage in pipes example

--- pipes/wc-async.js
+++ pipes/wc-async.js
@@ -5,19 +5,21 @@ const { EPOLLIN } = just.loop
 const { loop } = just.factory
 const BUFSIZE = 65536
 const buf = new ArrayBuffer(BUFSIZE)
-let size = 0
 const flags = sys.fcntl(STDIN_FILENO, sys.F_GETFL, 0) | O_NONBLOCK
 sys.fcntl(STDIN_FILENO, sys.F_SETFL, flags)
+let blocks = 0
 function onData (fd) {
   const n = read(fd, buf)
   if (n < 0) {
     error(`read: ${strerror(errno())} (${errno()})`)
     loop.remove(fd)
     return
-  } else if (n === 0) {
-    print(`size: ${size}`)
-    loop.remove(fd)
   }
-  size += n
+  if (n === 0) {
+    print(`size: ${blocks * BUFSIZE}`)
+    loop.remove(fd)
+    return
+  }
+  blocks++
 }
 loop.add(STDIN_FILENO, onData, EPOLLIN)
