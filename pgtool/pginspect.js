639f10ef15e5375cb204f7735a9deb47ae5a4d73
Andrew Johnston <apjohnsto@gmail.com>
2021-08-23 01:33:28 +0100

    remove memory examples. add size to pginspect example

--- pgtool/pginspect.js
+++ pgtool/pginspect.js
@@ -8,6 +8,7 @@ const packets = port
 for (const { packet, payload } of packets[direction]) {
   const keys = Object.keys(payload)
   if (!keys.length) continue
+  just.print(packet.bytes)
   just.print(packet.message.options.timestamp.tsval)
   just.print(binary.tcpDump(packet))
   just.print(binary.dump(Uint8Array.from(keys.map(k => payload[k]))))
